a3c8bda215dd8dc0a475daf080b1c94b3f799de1
danielbosa <danielbosa95@gmail.com>
2024-05-07 19:00:25 +0200

    Populated arrays

--- index.php
+++ index.php
@@ -4,14 +4,19 @@ include __DIR__ . "/Views/header.php";
 include __DIR__ . '/Models/Movie.php';
 include __DIR__ . '/Models/Book.php';
 
-
-$starWars = new MovieBasic('Star Wars',5);
+//$starWars = new MovieBasic('Star Wars',5);
 $movies = [
-    new Movie('Babylon A.D.','en','horrorTEST',9.99,5.601,'https://image.tmdb.org/t/p/w342/kt9nqD0uOar8IVE9191HXhWOXKI.jpg')
+    new Movie('Babylon A.D.','en','TEST',9.99,5.601,'https://image.tmdb.org/t/p/w342/kt9nqD0uOar8IVE9191HXhWOXKI.jpg'),
+    new Movie('Miraculous World : Paris, Les Aventures de Toxinelle et Griffe Noire','fr','TEST',4.99,6.935,'https://image.tmdb.org/t/p/w342/A46SghdyD9y7qQUxB5zOslospJ7.jpg'),
+    new Movie('Five Nights at Freddy\'s','en','TEST',2.99,7.8,'https://image.tmdb.org/t/p/w342/j9mH1pr3IahtraTWxVEMANmPSGR.jpg'),
+    new Movie('Rien à foutre','fr','TEST',0.99,6.417,'https://image.tmdb.org/t/p/w342/wLZ5jijVpOxHuBLmsICKLXwlldu.jpg')
 ];
 
 $books = [
-    new Book('Unlocking Android','W. Frank Ableson, Charlie Collins, Robi Sen',416,'Mobile',14.99,8.33,'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg')
+    new Book('Unlocking Android','W. Frank Ableson, Charlie Collins, Robi Sen',416,'Mobile',14.99,8.33,'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg'),
+    new Book('Android in Action, Second Edition','W. Frank Ableson", "Robi Sen',592,'Mobile',11.90,7.25,'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson2.jpg'),
+    new Book('Specification by Example','Gojko Adzic',0,'Mobile',0.00,5.48,'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/adzic.jpg'),
+    new Book('Flex 3 in Action','Tariq Ahmed with Jon Hirschi',600,'Internet',14.99,8.33,'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ahmed.jpg')
 ]
 
 ?>
